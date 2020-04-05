e826b7d1482935f7d5ab967b9f6d2d146c9dd819
Ayomide Ibosiola <Ayomide Ibosiola>
2020-04-05 12:33:03 -0700

    first commit

---
+++ src/components/Intro/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const Intro = props => (
+    <p className='App-intro'>
+  {props.message}  </p>
+  )
+  
+
+
+export default Intro;
